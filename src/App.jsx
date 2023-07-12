1acef09370c796e5215fcbba65bf3ab2324342c4
cw-kailas <130221927+cw-kailas@users.noreply.github.com>
2023-07-12 19:35:02 +0530

    magic commit �

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import "./App.css";
+import { Button } from "@abhi_d/basic-theme-setup.ui.buttons.button";
+import { Card } from "@cw-kailas/core.card/card";
+import { CardGroup } from "@cw-kailas/core.cardgroup";
+
+
+function App() {
+	return (
+		<div>
+            <Card></Card>
+			<Button>Click Me</Button>
+            <CardGroup />
+		</div>
+	);
+}
+
+export default App;
